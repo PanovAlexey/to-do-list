2ff3c1cd68c62a01c948a6b23f9f49390f48f3ad
Panov Alexey <alexeykapanov@gmail.com>
2020-12-26 05:11:00 +0300

    Create server.

--- main.go
+++ main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"log"
+	"todo-list/internal/app/servers"
+)
+
 func main() {
+	server := new(servers.Server)
+
+	if err := server.Run("8000"); err != nil {
+		log.Fatalf("error occurred while running http server: %s", err.Error())
+	}
 }
