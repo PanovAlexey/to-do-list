a8b97f6b0d76cc6547ea0e32d2aa9a565692d17a
Panov Alexey <alexeykapanov@gmail.com>
2021-03-26 09:11:00 +0300

    Create handlers.

---
+++ internal/app/presentation/auth.go
@@ -0,0 +1,11 @@
+package presentation
+
+import "github.com/gin-gonic/gin"
+
+func (h *Handler) signUp(c *gin.Context) {
+
+}
+
+func (h *Handler) signIn(c *gin.Context) {
+
+}
--- main.go
+++ main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"log"
+	"todo-list/internal/app/presentation"
 	"todo-list/internal/app/servers"
 )
 
 func main() {
+	handlers := new(presentation.Handler)
 	server := new(servers.Server)
 
-	if err := server.Run("8000"); err != nil {
+	if err := server.Run("8000", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
 }
