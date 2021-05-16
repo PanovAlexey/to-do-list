f0f13c2a87462fc32712920f8ff96f909db4b954
Panov Alexey <alexeykapanov@gmail.com>
2021-05-16 11:55:00 +0300

    Use error response in auth handler.

--- internal/app/presentation/auth.go
+++ internal/app/presentation/auth.go
@@ -1,9 +1,19 @@
 package presentation
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"todo-list/internal/app/domain/entity/user"
+	"todo-list/internal/app/presentation/responses"
+)
 
 func (h *Handler) signUp(c *gin.Context) {
+	var input user.User
 
+	if err := c.BindJSON(&input); err != nil {
+		responses.NewErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
 }
 
 func (h *Handler) signIn(c *gin.Context) {
