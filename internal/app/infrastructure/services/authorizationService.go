74bc738db3d4e35f7f5dd922694ec0bfee020c87
Panov Alexey <alexeykapanov@gmail.com>
2021-05-30 05:11:00 +0300

    Implement signing handler.

--- internal/app/infrastructure/services/authorizationService.go
+++ internal/app/infrastructure/services/authorizationService.go
@@ -3,15 +3,34 @@ package services
 import (
 	"crypto/sha1"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
 	"todo-list/internal/app/application/repositories"
 	"todo-list/internal/app/application/services"
 	"todo-list/internal/app/domain/entity/user"
 )
 
-const salt = "wertyuiopasdfghjkl"
+const (
+	salt       = "wertyuiopasdfghjkl"
+	tokenTTL   = 12 * time.Hour
+	signingKey = "qweqr78939870424&(#$@"
+)
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthorizationService struct {
 	repository repositories.AuthorizationRepositoryInterface
+}
+
+func NewAuthorizationService(repository repositories.AuthorizationRepositoryInterface) services.AuthorizationServiceInterface {
+	return &AuthorizationService{repository: repository}
+}
+
+func GetUser(username, password string) (user.User, error) {
+	return user.User{}, nil
 }
 
 func (service *AuthorizationService) CreateUser(user user.User) (int, error) {
@@ -19,8 +38,22 @@ func (service *AuthorizationService) CreateUser(user user.User) (int, error) {
 	return service.repository.CreateUser(user)
 }
 
-func NewAuthorizationService(repository repositories.AuthorizationRepositoryInterface) services.AuthorizationServiceInterface {
-	return &AuthorizationService{repository: repository}
+func (service *AuthorizationService) GenerateToken(username, password string) (string, error) {
+	user, err := service.repository.GetUser(username, service.generatePasswordHash(password))
+
+	if err == nil {
+		return "", err
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+			IssuedAt:  time.Now().Unix(),
+		},
+		user.Id,
+	})
+
+	return token.SignedString([]byte(signingKey))
 }
 
 func (service *AuthorizationService) generatePasswordHash(password string) string {
--- internal/app/presentation/auth.go
+++ internal/app/presentation/auth.go
@@ -3,6 +3,7 @@ package presentation
 import (
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"todo-list/internal/app/domain/dto"
 	"todo-list/internal/app/domain/entity/user"
 	"todo-list/internal/app/presentation/responses"
 )
@@ -28,5 +29,21 @@ func (h *Handler) signUp(c *gin.Context) {
 }
 
 func (h *Handler) signIn(c *gin.Context) {
+	var input dto.SignInInput
 
+	if err := c.BindJSON(&input); err != nil {
+		responses.NewErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	token, err := h.authorizationService.GenerateToken(input.Username, input.Password)
+
+	if err != nil {
+		responses.NewErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"token": token,
+	})
 }
