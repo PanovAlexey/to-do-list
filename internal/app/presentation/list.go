44a2cf5012381b211cb8c105bec7984af8f6eb2e
Panov Alexey <alexeykapanov@gmail.com>
2021-06-18 05:59:00 +0300

    Implement user identification endpoint by token.

--- internal/app/presentation/list.go
+++ internal/app/presentation/list.go
@@ -1,23 +1,25 @@
 package presentation
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
 
 func (h *Handler) createList(c *gin.Context) {
-
+	id, _ := c.Get(userCtx)
+	c.JSON(http.StatusOK, map[string]interface{}{
+		"id": id,
+	})
 }
 
 func (h *Handler) getAllLists(c *gin.Context) {
-
 }
 
 func (h *Handler) getListById(c *gin.Context) {
-
 }
 
 func (h *Handler) updateList(c *gin.Context) {
-
 }
 
 func (h *Handler) deleteList(c *gin.Context) {
-
 }
